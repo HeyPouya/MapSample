4a0e7be1ee16296217266e25957a4cbd02d1fc80
Pouya Heydari <pouyaheydary@gmail.com>
2020-07-09 18:11:10 +0430

    configured the entity of the vehicle table for room

--- app/src/main/java/com/pouyaheydari/sample/map/android/pojo/VehiclesData.kt
+++ app/src/main/java/com/pouyaheydari/sample/map/android/pojo/VehiclesData.kt
@@ -1,12 +1,17 @@
 package com.pouyaheydari.sample.map.android.pojo
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class VehiclesData(val vehicles: List<Vehicle>)
 
+@Entity
 @JsonClass(generateAdapter = true)
 data class Vehicle(
+    @PrimaryKey(autoGenerate = true)
+    val uid: Int,
     val bearing: Float,
     val image_url: String,
     val lat: Double,

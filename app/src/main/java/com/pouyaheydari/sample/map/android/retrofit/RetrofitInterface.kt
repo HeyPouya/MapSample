5ff769ecbaa937410960975b71680e91a2211aca
Pouya Heydari <pouyaheydary@gmail.com>
2020-07-09 16:18:02 +0430

    added getVehicles() to RetrofitInterface

--- app/src/main/java/com/pouyaheydari/sample/map/android/retrofit/RetrofitInterface.kt
+++ app/src/main/java/com/pouyaheydari/sample/map/android/retrofit/RetrofitInterface.kt
@@ -1,8 +1,14 @@
 package com.pouyaheydari.sample.map.android.retrofit
 
+import com.pouyaheydari.sample.map.android.pojo.VehiclesData
+import retrofit2.http.GET
+
 /**
  * Retrofit main interface
  *
  */
 interface RetrofitInterface {
+
+    @GET("assets/test/document.json")
+    suspend fun getVehicles(): VehiclesData
 }

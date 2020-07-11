dea6ff5d9d665882ab9d5a0166571e0dab0d59aa
Pouya Heydari <pouyaheydary@gmail.com>
2020-07-11 10:48:31 +0430

    added unit test for SplashViewModel

---
+++ app/src/test/java/com/pouyaheydari/sample/map/android/features/FakeDataRepository.kt
@@ -0,0 +1,20 @@
+package com.pouyaheydari.sample.map.android.features
+
+import com.pouyaheydari.sample.map.android.pojo.Vehicle
+import com.pouyaheydari.sample.map.android.pojo.VehiclesData
+import com.pouyaheydari.sample.map.android.repository.DataRepositoryInterface
+
+class FakeDataRepository : DataRepositoryInterface {
+
+    private val vehiclesList: MutableList<Vehicle> = emptyList<Vehicle>().toMutableList()
+
+    override suspend fun getVehicles(): VehiclesData = VehiclesData(vehiclesList)
+
+    override suspend fun getOfflineVehicles(): List<Vehicle> = vehiclesList
+
+    override suspend fun deleteAllVehicles() {}
+
+    override suspend fun insertVehicle(vehicle: List<Vehicle>) {
+        vehiclesList.addAll(vehicle)
+    }
+}
---
+++ app/src/test/java/com/pouyaheydari/sample/map/android/features/splash/SplashViewModelTest.kt
@@ -0,0 +1,93 @@
+package com.pouyaheydari.sample.map.android.features.splash
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.pouyaheydari.sample.map.android.MainCoroutineRule
+import com.pouyaheydari.sample.map.android.features.FakeDataRepository
+import com.pouyaheydari.sample.map.android.getOrAwaitValue
+import com.pouyaheydari.sample.map.android.pojo.Vehicle
+import com.pouyaheydari.sample.map.android.pojo.ViewNavigationEnum
+import com.pouyaheydari.sample.map.android.repository.DataRepositoryInterface
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.core.Is.`is`
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+internal class SplashViewModelTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    private lateinit var viewModel: SplashViewModel
+    private lateinit var repository: DataRepositoryInterface
+    private val vehicleList = listOf(
+        Vehicle(
+            null,
+            53F,
+            "https://snapp.ir/assets/test/snapp_map@2x.png",
+            35.7575154,
+            51.4104956,
+            "ECO"
+        ),
+        Vehicle(
+            null,
+            205F,
+            "https://snapp.ir/assets/test/snapp_map@2x.png",
+            35.7580966,
+            51.4094662,
+            "ECO"
+        ),
+        Vehicle(
+            null,
+            324F,
+            "https://snapp.ir/assets/test/snapp_map_st2.png",
+            35.7577213,
+            51.4092553,
+            "PLUS"
+        )
+    )
+
+    @Before
+    fun setUp() {
+        repository = FakeDataRepository()
+        viewModel = SplashViewModel(repository)
+    }
+
+    @Test
+    fun decideNextView_networkAvailable_expectedMap() {
+        val isNetworkAvailable = true
+
+        viewModel.decideNextView(isNetworkAvailable)
+
+        viewModel.getNextPageLiveData().getOrAwaitValue()
+        assertThat(viewModel.getNextPageLiveData().value, `is`(ViewNavigationEnum.MAP))
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun decideNextView_networkUnAvailable_expectedVehicleList() {
+        val isNetworkAvailable = false
+        runBlockingTest { repository.insertVehicle(vehicleList) }
+
+        viewModel.decideNextView(isNetworkAvailable)
+
+        viewModel.getNextPageLiveData().getOrAwaitValue()
+        assertThat(viewModel.getNextPageLiveData().value, `is`(ViewNavigationEnum.VEHICLE_LIST))
+    }
+
+    @Test
+    fun decideNextView_networkUnAvailable_expectedOfflineNoData() {
+        val isNetworkAvailable = false
+
+        viewModel.decideNextView(isNetworkAvailable)
+
+        viewModel.getNextPageLiveData().getOrAwaitValue()
+        assertThat(viewModel.getNextPageLiveData().value, `is`(ViewNavigationEnum.OFFLINE_NO_DATA))
+    }
+}

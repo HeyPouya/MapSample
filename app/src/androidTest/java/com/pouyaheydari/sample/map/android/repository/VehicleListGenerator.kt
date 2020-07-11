49e18f09d73bee707d325cd635637673f73e53dd
Pouya Heydari <pouyaheydary@gmail.com>
2020-07-11 16:08:42 +0430

    Added tests for room

---
+++ app/src/androidTest/java/com/pouyaheydari/sample/map/android/repository/VehicleListGenerator.kt
@@ -0,0 +1,30 @@
+package com.pouyaheydari.sample.map.android.repository
+
+import com.pouyaheydari.sample.map.android.pojo.Vehicle
+
+fun getVehiclesList() = listOf(
+    Vehicle(
+        null,
+        53F,
+        "https://snapp.ir/assets/test/snapp_map@2x.png",
+        35.7575154,
+        51.4104956,
+        "ECO"
+    ),
+    Vehicle(
+        null,
+        205F,
+        "https://snapp.ir/assets/test/snapp_map@2x.png",
+        35.7580966,
+        51.4094662,
+        "ECO"
+    ),
+    Vehicle(
+        null,
+        324F,
+        "https://snapp.ir/assets/test/snapp_map_st2.png",
+        35.7577213,
+        51.4092553,
+        "PLUS"
+    )
+)
